4d3283d63d2c3e9ff3004c4b3e3946f329bbbb80
Alexandra Mallet <malletalexandra@gmail.com>
2022-09-11 09:43:41 +0200

    routes user

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const userCtr = require("../controllers/user");
+
+router.post("/signup", userCtr.signup);
+router.post("/login", userCtr.login);
+
+module.exports = router;
