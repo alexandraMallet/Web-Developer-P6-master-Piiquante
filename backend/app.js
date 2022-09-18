11b2dd4b2634417c3da336e90e37809f46ada975
Alexandra Mallet <malletalexandra@gmail.com>
2022-09-18 10:06:33 +0200

    ajout multer

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
 const dotenv = require("dotenv").config();
+const path = require("path");
 
 //TODO : helmet
 //TODO :  rateLimit
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", sauceRoutes);
+app.use("images", express.static(path.join(__dirname, "images")));
 
 
 module.exports = app; 
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const sauceCrt = require("../controllers/sauce");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 
-router.post("/", auth, sauceCrt.createSauce);
+router.post("/", auth, multer, sauceCrt.createSauce);
 router.get("/", auth, sauceCrt.getAllSauces);
 router.get("/:id", auth, sauceCrt.getOneSauce);
-router.put("/:id", auth, sauceCrt.modifySauce);
+router.put("/:id", auth, multer, sauceCrt.modifySauce);
 router.delete("/:id", auth, sauceCrt.deleteSauce);
 router.post("/:id/like", auth, sauceCrt.likeDislikeSauce);
 
