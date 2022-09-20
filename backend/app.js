3511b05a06150529fb7b9899432caeace56cc889
Alexandra Mallet <malletalexandra@gmail.com>
2022-09-20 19:33:59 +0200

    helmet ok

--- backend/app.js
+++ backend/app.js
@@ -6,33 +6,34 @@ const dotenv = require("dotenv").config();
 const path = require("path");
 const helmet = require("helmet");
 
-//TODO : helmet
 //TODO :  rateLimit
 
 const app = express();
 
 app.use(express.json());
+
+
 app.use(
-    helmet({
-      contentSecurityPolicy: false,
-    })
-  );
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
 mongoose.connect(process.env.URL_DATABASE,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("Application connectée à MongoDB."))
-    .catch(() => console.log("Impossible de se connecter à MongoDB."));
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Application connectée à MongoDB."))
+  .catch(() => console.log("Impossible de se connecter à MongoDB."));
 
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use("/api/auth", userRoutes);
